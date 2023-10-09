487983d160f9faf28feebf2fc0c3b6c1ce230390
dado-offbrand <dogeychicken47@gmail.com>
2023-10-09 18:21:27 -0400

    Fixed naming conventions

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     static double[] rectInfo = new double[2];
 
     public static void main(String[] args) {
-        GetInput("What is the length of the first side?", 0);
-        GetInput("What is the length of the second side?", 1);
+        getInput("What is the length of the first side?", 0);
+        getInput("What is the length of the second side?", 1);
 
         System.out.printf("The area of your rectangle is: %.02f\n", rectInfo[0] * rectInfo[1]);
         System.out.printf("The perimeter of your rectangle is: %.02f", (rectInfo[0] * 2) + (rectInfo[1]*2));
     }
 
-    private static void GetInput(String question, int outputIndex) {
+    private static void getInput(String question, int outputIndex) {
         Scanner scanner = new Scanner(System.in);
         boolean verify = false;
 
